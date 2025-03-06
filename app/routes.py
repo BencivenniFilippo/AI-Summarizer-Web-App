c846bbe1d1e3d246b00a1ea72b0e3901a537b812
Sbencio <filibenci35@gmail.com>
2025-03-06 18:51:42 +0100

    error pages and jinja playground

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,14 @@ main_blueprint = Blueprint('main', __name__) # create a blueprint named 'main'
 # define a route inside the blueprint
 @main_blueprint.route('/')
 def home():
-    return render_template('home.html')
+    input = 'testo'
+    return render_template('home.html', shown_input=input)
+
+@main_blueprint.route('/playground')
+def playground():
+    l = [1, 2, 'giovanni', 4]
+
+    return render_template('stuff.html', lista=l)
 
 # ADDING HISTORY
 # @main.route('/history')
