8ebf917906624750c886c3f5f3a1fe1e0a38d096
Sbencio <filibenci35@gmail.com>
2025-03-06 18:59:41 +0100

    merge solution 2

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,6 @@ main_blueprint = Blueprint('main', __name__) # create a blueprint named 'main'
 # define a route inside the blueprint
 @main_blueprint.route('/')
 def home():
-<<<<<<< HEAD
     input = 'testo'
     return render_template('home.html', shown_input=input)
 
@@ -14,9 +13,6 @@ def playground():
     l = [1, 2, 'giovanni', 4]
 
     return render_template('stuff.html', lista=l)
-=======
-    return render_template('home.html')
->>>>>>> 03b32e032067faf84c84bea0529226db3449b767
 
 # ADDING HISTORY
 # @main.route('/history')
