94fa7b2a632fb9f91f10d791c7536a45cf23bd60
Sbencio <filibenci35@gmail.com>
2025-03-09 23:34:39 +0100

    lighter BART model

--- app/nlp.py
+++ app/nlp.py
@@ -1,6 +1,6 @@
 from transformers import BartForConditionalGeneration, BartTokenizer
 
-model_name = "facebook/bart-large-cnn"
+model_name = "sshleifer/distilbart-cnn-12-6"
 tokenizer = BartTokenizer.from_pretrained(model_name)
 model = BartForConditionalGeneration.from_pretrained(model_name)
 
@@ -10,19 +10,19 @@ def summarize(text):
     if length <= 50:
         max_length = 30
         min_length = 5
+        length_penalty = 1.0
+        num_beams = 4
+
+    elif length <= 300:
+        max_length = 100
+        min_length = 20
         length_penalty = 1.5
         num_beams = 5
 
-    elif length <= 300:
-        max_length = 130
-        min_length = 25
-        length_penalty = 2.0
-        num_beams = 5
-
     else:
-        max_length = 200
-        min_length = 50
-        length_penalty = 2.5
+        max_length = 150
+        min_length = 40
+        length_penalty = 2.0
         num_beams = 5
 
     inputs = tokenizer.encode(text, return_tensors="pt", max_length=1024, truncation=True)
